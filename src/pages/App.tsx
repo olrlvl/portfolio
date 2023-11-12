815a9f9d49395d40760f34c8cf72b13f9c9b2410
olrlvl <tlsgutjq94@naver.com>
2023-11-12 20:17:23 +0900

    💄 슬라이드 이전, 다음 버튼 및 스크롤 아이콘 기초 작업

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+
 import { Swiper, SwiperSlide } from "swiper/react"; // basic
 import SwiperCore from "swiper";
 
@@ -6,30 +7,43 @@ import "swiper/css"; //basic
 
 function App() {
     const swiperRef = useRef<SwiperCore>();
-    return (
-        <main className="bg-gray-300 w-full pt-[72px] flex flex-col relative">
-            <section className="bg-green-200 py-8 flex flex-col items-center justify-center gap-3">
-                <h2 className="text-4xl">프로젝트 제목 소개</h2>
-                <p>기여도 및 부연설명 부분</p>
 
-                <Swiper
-                    onSwiper={(swiper) => {
-                        swiperRef.current = swiper;
-                    }}
-                    className="w-[1000px] h-[600px]"
-                    spaceBetween={50}
-                    slidesPerView={"auto"}
-                    scrollbar={{ draggable: true }}
-                    navigation
-                    pagination={{ clickable: true }}
-                >
-                    <SwiperSlide className="bg-black">Slide 1</SwiperSlide>
-                    <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
-                    <SwiperSlide className="bg-green-500">Slide 3</SwiperSlide>
-                    <SwiperSlide className="bg-yellow-500">Slide 4</SwiperSlide>
-                </Swiper>
+    return (
+        <main className="bg-gray-300 w-full h-screen pt-[72px] flex flex-col relative">
+            {/* Intro Section */}
+            <section className="bg-green-200 py-8 flex flex-col items-center justify-center h-full">
+                <h2 className="text-4xl">프로젝트 제목 소개</h2>
+                <p className="mt-3">기여도 및 부연설명 부분</p>
+
+                <div className="flex h-[600px] items-center gap-4 mt-3">
+                    {/* Prev Button */}
+                    <div className="icon-xl bg-red-200 cursor-pointer" onClick={() => swiperRef.current?.slidePrev()}></div>
+                    {/* Slide */}
+                    <Swiper
+                        onSwiper={(swiper) => {
+                            swiperRef.current = swiper;
+                        }}
+                        className="w-[1000px] h-full z-0"
+                        spaceBetween={50}
+                        centeredSlides={true}
+                        centerInsufficientSlides={true}
+                        slidesPerView={"auto"}
+                        navigation
+                        pagination={{ clickable: true }}
+                    >
+                        <SwiperSlide className="bg-black">Slide 1</SwiperSlide>
+                        <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
+                        <SwiperSlide className="bg-green-500">Slide 3</SwiperSlide>
+                        <SwiperSlide className="bg-yellow-500">Slide 4</SwiperSlide>
+                    </Swiper>
+                    {/* Next Button */}
+                    <div className="icon-xl bg-red-200 cursor-pointer" onClick={() => swiperRef.current?.slideNext()}></div>
+                </div>
+
+                <div className="icon-xl bg-black cursor-pointer mt-8"></div>
             </section>
-            <section className="bg-gray-500 h-96 flex flex-col items-center">스크롤을 내렸을때</section>
+
+            <section className="bg-gray-500 flex flex-col items-center">스크롤을 내렸을때</section>
         </main>
     );
 }
