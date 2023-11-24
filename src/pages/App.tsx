3a10e5fe85052175852cb15cb015ef9baa485b31
olrlvl <tlsgutjq94@naver.com>
2023-11-24 17:22:09 +0900

    💄 더미 이미지 추가 및 슬라이더 스타일 수정

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,15 +5,20 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import SwiperCore from "swiper";
 import "swiper/css";
 
-const Container = tw.div`
-    flex
-    items-center
-    justify-center
-    flex-col
-    w-full
-    h-13
-    bg-indigo-600
-`;
+// asset dummy img
+import dummy1 from "@src/assets/img/Slide1.jpg";
+import dummy2 from "@src/assets/img/Slide2.jpg";
+import dummy3 from "@src/assets/img/Slide3.jpg";
+
+// const Container = tw.div`
+//     flex
+//     items-center
+//     justify-center
+//     flex-col
+//     w-full
+//     h-13
+//     bg-indigo-600
+// `;
 
 function App() {
     const swiperRef = useRef<SwiperCore>();
@@ -30,10 +35,12 @@ function App() {
                 render={({ fullpageApi }) => {
                     return (
                         <ReactFullpage.Wrapper>
-                            <section className="section bg-white w-full flex flex-col relative pt-[72px]">
+                            <section className="section bg-white w-full flex flex-col relative">
                                 {/* Intro Section */}
-                                <section className="bg-white py-8 flex flex-col items-center justify-center h-full" data-achor="slide1">
+                                <section className="bg-white flex flex-col items-center " data-achor="slide1">
+                                    {/* 슬라이퍼 인덱스에 따라서 제목 변경 */}
                                     <h2 className="text-4xl">프로젝트 제목 소개</h2>
+                                    {/* 설명도 마찬가지 */}
                                     <p className="mt-3">기여도 및 부연설명 부분</p>
 
                                     <div className="flex h-[650px] items-center gap-4 mt-3">
@@ -42,7 +49,7 @@ function App() {
                                         {/* Slide */}
                                         <Swiper
                                             onSwiper={(swiper) => (swiperRef.current = swiper)}
-                                            className="w-[calc(100vw-64px)] h-full z-0"
+                                            className="w-[calc(100vw-64px)] h-full z-0 py-4"
                                             spaceBetween={32}
                                             centeredSlides={true}
                                             slidesPerView={2.5}
@@ -52,18 +59,32 @@ function App() {
                                             autoplay={{ delay: 3000 }}
                                             pagination={{ clickable: true }}
                                         >
-                                            <SwiperSlide className="bg-black">Slide 1</SwiperSlide>
-                                            <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
-                                            <SwiperSlide className="bg-green-500">Slide 3</SwiperSlide>
-                                            <SwiperSlide className="bg-black">Slide 1</SwiperSlide>
-                                            <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
-                                            <SwiperSlide className="bg-green-500">Slide 3</SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box rounded-xl overflow-hidden">
+                                                <img className="w-full" src={dummy1} alt="first project" />
+                                            </SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box rounded-xl overflow-hidden">
+                                                <img className="w-full" src={dummy2} alt="second project" />
+                                            </SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box rounded-xl overflow-hidden">
+                                                <img className="w-full" src={dummy3} alt="third project" />
+                                            </SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box">
+                                                <img className="w-full rounded-xl" src={dummy1} alt="first project" />
+                                            </SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box rounded-xl overflow-hidden">
+                                                <img className="w-full" src={dummy2} alt="second project" />
+                                            </SwiperSlide>
+                                            <SwiperSlide className="flex shadow-select-box rounded-xl overflow-hidden">
+                                                <img className="w-full" src={dummy3} alt="third project" />
+                                            </SwiperSlide>
                                         </Swiper>
                                         {/* Next Button */}
                                         {/* <div className="icon-xl bg-red-200 cursor-pointer" onClick={() => swiperRef.current?.slideNext()}></div> */}
                                     </div>
 
-                                    <div className="icon-xl bg-black cursor-pointer mt-8"></div>
+                                    <div className="flex icon-xl bg-red-200 cursor-pointer absolute bottom-8" onClick={() => fullpageApi.moveSectionDown()}>
+                                        Arrow Icon
+                                    </div>
                                 </section>
                             </section>
 
